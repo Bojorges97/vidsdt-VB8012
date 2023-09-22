118470b60483524e11d12f3364e7e8efa65ba718
Bojorges97 <davibojorges@gmail.com>
2023-09-22 16:44:10 -0600

    test

--- Version_Hollow_Purple/Backend/python/Gaussian_pulse.py
+++ Version_Hollow_Purple/Backend/python/Gaussian_pulse.py
@@ -4,11 +4,11 @@ import asyncio
 from pyvirtualbench import PyVirtualBench, PyVirtualBenchException, FGenWaveformMode
 from waveform import GaussianPulse
 from websockets.server import serve
-from numpy import array, savetxt
+from numpy import array, savetxt, arange
 import json
-# from matplotlib import pyplot as plt # Debug
+from matplotlib import pyplot as plt # Debug
 
-def createGaussianPulse(inputs):
+def triggerWaveform(inputs):
     pulse = GaussianPulse(inputs['amplitude'], inputs['frecuency'], inputs['nSamples'], inputs['fs'], inputs['hgw'])
     pulse.getFFT() 
 
@@ -21,15 +21,36 @@ def createGaussianPulse(inputs):
     fgen.stop()
     fgen.configure_arbitrary_waveform(pulseArray.tolist(), sample_rate)#Sample rate 100S/s=>0.01 100000=>0.00001
     fgen.run()
+    
     # fgen.release()
 
+def msoData():
+    mso = virtualbench.acquire_mixed_signal_oscilloscope()
+
+    mso.auto_setup()
+
+    # sample_rate, acquisition_time, pretrigger_time, sampling_mode = mso.query_timing()
+    # channels = mso.query_enabled_analog_channels()
+    # channels_enabled, number_of_channels = virtualbench.collapse_channel_string(channels)
+
+    mso.run()
+
+    # Read the data by first querying how big the data needs to be, allocating the memory, and finally performing the read.
+    analog_data = mso.read_analog_digital_u64()[0]
+    # testArray = array(analog_data)# Debug
+    # w = arange(0, testArray.size, 1, dtype=float)# Debug
+    # # # print(testArray)# Debug
+    # # # savetxt('test.txt', testArray) # Debug
+    # plt.plot(w, testArray, color="red") # Debug
+    # plt.show() # Debug
+    return analog_data
 async def echo(websocket):
     async for message in websocket:
         msg = json.loads(message)
         print(msg)
         try:
-            print("Hi")
-        #   createGaussianPulse(msg)
+           triggerWaveform(msg)
+           await websocket.send(json.dumps({'data' : msoData()}))
         except PyVirtualBenchException as e:
             print("Error/Warning %d occurred\n%s" % (e.status, e))
         finally:
@@ -38,11 +59,12 @@ async def echo(websocket):
 
 if __name__ == '__main__':
     global virtualbench, fgen
-    # virtualbench = PyVirtualBench('VB8012-31C033D')
-    # fgen = virtualbench.acquire_function_generator()
+    virtualbench = PyVirtualBench('VB8012-31C033D')
+    # msoData() # debug
+    fgen = virtualbench.acquire_function_generator()
     async def main():
         print("Service Up....")
-        async with serve(echo, "0.0.0.0", 80):
+        async with serve(echo, "0.0.0.0", 1025):
             await asyncio.Future()  # run forever
 
     asyncio.run(main())
--- Version_Hollow_Purple/Frontend/JS/GaussianPulse.js
+++ Version_Hollow_Purple/Frontend/JS/GaussianPulse.js
@@ -1,9 +1,176 @@
-var websocket = new WebSocket("ws://172.16.29.60:80/");
+let canvas = document.querySelector("canvas#osciloscope");
+let Status = document.getElementById('id_status');
+// let inputElem = document.getElementById('id_buffers');
+let ip_address = window.location.hostname;
+console.log(ip_address)
+let webSocket = new WebSocket("ws://" + ip_address + ":1025/");
+console.log("David was Here");
+console.log("Version Hollow_purple");
 
 let data = {};
+
+webSocket.onmessage = function(info) {
+    let data = JSON.parse(info.data);
+    console.log(data)
+    if (data["data"]){
+        // console.log(JSON.parse(data["data"]))
+        plotData(data["data"]);
+        return false
+    }
+}
 
 function sendmsg(steps){
     steps.forEach(element =>data[element.name]=element.valueAsNumber);
     console.log(data);
-    websocket.send(JSON.stringify(data));
+    webSocket.send(JSON.stringify(data));
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+canvas.width = canvas.parentElement.clientWidth;
+canvas.height =  6000;
+canvas.lineWidth = 0.5;
+// canvas.numBuffers = 1;
+canvas.baudRate = 0;
+canvas.data = [];
+canvas.allData = [];
+
+window.addEventListener('resize', function(event){
+    canvas.width = canvas.parentElement.clientWidth;
+    canvas.height = canvas.parentElement.clientHeight-100;
+})
+
+// inputElem.addEventListener('input', () => {
+//     canvas.numBuffers = parseInt(inputElem.value)
+// });
+
+webSocket.onopen = function(frame) {
+    Status.innerText = "Conectado " + ip_address + ":8080";
+    console.log("Connected to: " + ip_address + ":8080")
+};
+
+webSocket.onclose = function(frame) {
+    Status.innerText = "Socket desconectado";
+    console.log("Socket disconnected ")
+};
+
+function plotData(data) {
+    let ctx = canvas.getContext("2d");
+    canvas.data.push(data);
+    canvas.allData.push(data);
+    canvas.baudRate = Math.floor(canvas.allData.flat(1).length/canvas.allData.length);
+    // document.getElementById("id_baudRate").innerText = "BaudRate :" + canvas.baudRate;
+    // while(canvas.data.length>canvas.numBuffers){
+    //     canvas.data.shift();
+    // }
+
+    let dataMap = mapCanvas(canvas.data.flat(1),canvas.width, canvas.height);
+    window.data = canvas.data;
+    clearCanvas();
+    window.dataMap = dataMap;
+    ctx.beginPath();
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = canvas.lineWidth;
+    dataMap.forEach(function insertElement(currentValue, index, array){
+        let X = currentValue[0];
+        let Y = currentValue[1];
+        ctx.lineTo(X, Y);
+    });
+    ctx.stroke();
+    getFileSize();
+}
+
+
+
+
+function getFileSize(){
+    let sizeLabel = document.getElementById('id_sizeFile');
+    let size = canvas.allData.flat(1).length/1024*4;
+    size = size.toPrecision(4);
+    sizeLabel.innerText = size + "KB";
+}
+
+
+
+
+function mapCanvas(array,maxX, maxY){
+    
+    let values = [];
+    let maxXOld = array.length;
+    let maxYOld = 1200;
+
+    for(let index=0; index<array.length; index++){
+        let Y = (maxYOld-array[index])*maxY/maxYOld;
+        let X =                (index)*maxX/maxXOld;
+        values.push([X,Y]);
+    }
+    return values;
+}
+
+
+
+
+function clearCanvas() {
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgba(0,0,0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgba(0,0,0)';
+
+    for(let i=1; i<5; i++){
+        ctx.beginPath();
+        ctx.strokeStyle = "white";
+        ctx.moveTo(0,  i*canvas.height/5);
+        ctx.lineTo(canvas.width, i*canvas.height/5);
+        ctx.lineWidth = 0.6;
+        ctx.stroke();
+    }
+
+    ctx.beginPath();
+    ctx.strokeStyle = "#9B9B9B";
+    ctx.moveTo(0,0);
+    ctx.lineTo(0,canvas.height);
+    ctx.lineTo(canvas.width,canvas.height);
+    ctx.lineTo(canvas.width,0);
+    ctx.lineTo(0,0);
+    ctx.lineWidth = 5;
+    ctx.stroke();
+}
+
+
+
+
+
+function saveFile(){
+let datos = JSON.stringify(canvas.allData);
+datos = datos.replaceAll(',','\n');
+datos = datos.replaceAll('[','');
+datos = datos.replaceAll(']','');
+let blob = new Blob([datos], {type: "text/txt"});
+
+let url = window.URL.createObjectURL(blob);
+let anchor = document.createElement("a");
+anchor.href = url;
+let nameFile = document.getElementById('id_nameFile').value;
+if(!nameFile.length)
+    anchor.download = "demo.txt";
+else
+    anchor.download = (nameFile + ".txt");
+console.log(anchor)
+anchor.click();
+window.URL.revokeObjectURL(url);
+anchor.remove();
+}
+
+
+
+
+function clearFile(){
+    clearCanvas();
+    canvas.data = [];
+    canvas.allData = [];
+    canvas.baudRate = 0;
 }
