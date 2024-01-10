dd857582d738f1b39fa13e6ce4e66ff82e68e0f0
Bojorges97 <davibojorges@gmail.com>
2024-01-10 07:08:14 -0600

    test

--- Version_Infinity_Void/Frontend/JS/GaussianPulse.js
+++ Version_Infinity_Void/Frontend/JS/GaussianPulse.js
@@ -1,4 +1,3 @@
-
 console.log("David was Here");
 console.log("Version Infinity Void");
 
@@ -10,14 +9,10 @@ console.log("Version Infinity Void");
 
 let ip_address = window.location.hostname,
     webSocket,
-    frontData = {},
-    backData;
+    storageData = getData("storage"),
+    plotsData = getData("plots");
 
-let today = new Date(),
-    expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000), // Plus 30 days
-    labels = ['amplitude', 'frecuency', 'nSamples', 'fs', 'hgw']; // Inputs for cookies
-
-const Status = document.getElementById('id_status'),
+const Status = document.getElementById("id_status"),
       TimePlotInput = document.querySelector("#timePlotIn"),
       FrecPlotInput = document.querySelector("#frecuencyPlotIn");
 
@@ -27,9 +22,11 @@ const Status = document.getElementById('id_status'),
 //////////                      ////////////////
 /////////////////////////////////////////////**/
 
-labels.forEach(element => {element != getCookie(element) ? document.querySelector('#'+element).value = getCookie(element) : {} ;})
+Object.entries(storageData).forEach((key, value) => {document.querySelector("#"+key).value = value});
 
 connectWebSockets();
+
+drawPlots();
 
 /**/////////////////////////////////////////////
 //////////                       ///////////////
@@ -37,10 +34,18 @@ connectWebSockets();
 //////////                       ///////////////
 /////////////////////////////////////////////**/
 
+/**
+  * Function to confirm the conection with the server.
+  *
+**/
 webSocket.onopen = function() {
     Status.innerText = "Conectado a " + ip_address + ":5500";
 };
 
+/**
+  * Function to confirm the disconection with the server and reconnect with websocket.
+  *
+**/
 webSocket.onclose = function() {
     Status.innerText = "Socket desconectado";
     setTimeout(function() {
@@ -48,85 +53,48 @@ webSocket.onclose = function() {
     }, 2000);// Reconnect websocket after two seconds
 };
 
+/**
+  * Function to retreive backend data and update plots in the interfaz.
+  *
+  * @param {object} info Dictionary with data about the Virtual Bench.
+**/
 webSocket.onmessage = function (info) {
-    backData = JSON.parse(info.data);
-    console.log(backData)
+    plotsData = JSON.parse(info.data);
+    console.log(plotsData)
     
-    if(backData['connectDev']){
-      document.querySelector('tr.gen-buttons').style.display = ''; 
+    if(plotsData["connectDev"]){
+      document.querySelector("div.gen-buttons").style.visibility = "visible"; 
     } else {
-      document.querySelector('tr.gen-buttons').style.display = 'none'; 
-      document.querySelector('#timePlotIn').innerHTML = ''; 
-      document.querySelector('#frecuencyPlotIn').innerHTML = ''; 
+      document.querySelector("div.gen-buttons").style.visibility = "hidden"; 
+      document.querySelector("#timePlotIn").innerHTML = ""; 
+      document.querySelector("#frecuencyPlotIn").innerHTML = ""; 
     }
-
-    if (backData["fGen"]){
-      document.querySelector('#reloadButton').style.display = ''; 
-      let TimePlotInput ={
-          divId : 'timePlotIn', 
-          dataX : backData["fGen"]["t"],
-          dataY : backData["fGen"]["timeY"],
-          title : 'Señal en el tiempo',
-          axisX : 't [s]',
-          axisY : 'f(t) [V]', 
-      };
-      createPlot(TimePlotInput);
-      let FrecPlotInput ={
-        divId : 'frecuencyPlotIn', 
-        dataX : backData["fGen"]["w"],
-        dataY : backData["fGen"]["frecY"],
-        title : 'Señal en la frecuencia',
-        axisX : 'w [Hz]',
-        axisY : 'f(w) [V]',
-      };
-      createPlot(FrecPlotInput);
-    }
-
-    if (backData["fmso"]){
-      let TimePlotOut ={
-        divId : 'timePlotOut', 
-        dataX : backData["fmso"]["t"],
-        dataY : backData["fmso"]["timeY"],
-        title : 'Señal en el tiempo',
-        axisX : 't [s]',
-        axisY : 'f(t) [V]', 
-      };
-      createPlot(TimePlotOut);
-      let FrecPlotOut ={
-        divId : 'frecuencyPlotOut', 
-        dataX : backData["fmso"]["w"],
-        dataY : backData["fmso"]["frecY"],
-        title : 'Señal en la frecuencia',
-        axisX : 'w [Hz]',
-        axisY : 'f(w) [V]',
-      };
-      createPlot(FrecPlotOut);
-    } 
-
-    if(backData['average']){
-      let timePlotAverage ={
-        divId : 'timePlotAverage', 
-        dataX : backData["average"]["t"],
-        dataY : backData["average"]["timeY"],
-        title : 'Señal en el tiempo',
-        axisX : 't [s]',
-        axisY : 'f(t) [V]', 
-      };
-      createPlot(timePlotAverage);
-    }
+    drawPlots();
+    setData("plots", plotsData)
 };
 
+/**
+  * Function to reconnect with websocket.
+  *
+**/
 function connectWebSockets(){
     webSocket = new WebSocket("ws://" + ip_address + ":1025/");
 }
 
+/**
+  * Function to send requests to backend.
+  *
+  * @param {object} button Button pressed by the user.
+  * @param {object} inputs Virtual Bench configuration inputs.
+**/
 function sendmsg(button, inputs){
-    frontData['button'] = button
+    storageData["button"] = button;
     inputs.forEach(element => {
-      element.name != 'model'? frontData[element.name] = element.valueAsNumber : frontData[element.name] = element.value;
-      setCokie(element.name, element.value)
+      element.name != "model"? storageData[element.name] = element.valueAsNumber : storageData[element.name] = element.value;
     });
-    webSocket.send(JSON.stringify(frontData));
+    webSocket.send(JSON.stringify(storageData));
+    delete storageData["button"];
+    setData("plots", storageData);
 }
 
 /**/////////////////////////////////////////////
@@ -135,30 +103,33 @@ function sendmsg(button, inputs){
 //////////                       ///////////////
 /////////////////////////////////////////////**/
 
-function createPlot(featuresPlot){
-    Plotly.newPlot( featuresPlot['divId'], [{
-      x: featuresPlot['dataX'],
-      y: featuresPlot['dataY']}], {
+/**
+  * Function to create plots in the interfaz.
+  *
+  * @param {object} plotFeatures Plot features dictionary.
+**/
+function createPlot(plotFeatures){
+    Plotly.newPlot( plotFeatures["divId"], [{
+      x: plotFeatures["dataX"],
+      y: plotFeatures["dataY"]}], {
           margin: { t: 0 },
           title:{
-              text:featuresPlot['title'],
+              text:plotFeatures["title"],
               x: 0.5,
               automargin: true,
               font: {
                 family: "Arial",
                 size: 15,
-                color: 'black'
+                color: "black"
               }
           },  
-          width: 800,
-          height: 250,
           xaxis: { 
               title: {
-                text: featuresPlot['AxisX'],
+                text: plotFeatures["AxisX"],
                 font: {
                   family: "Arial",
                   size: 15,
-                  color: 'black'
+                  color: "black"
                 },
               },
               position: 0.3
@@ -167,36 +138,104 @@ function createPlot(featuresPlot){
           yaxis: {
               title: {
                   family: "Arial",
-                  text: featuresPlot['AxisY'],
+                  text: plotFeatures["AxisY"],
                   font: {
                     size: 15,
-                    color: 'black'
+                    color: "black"
                   }
                 }
           }
-      } 
+      },
+      {responsive: true}
     );
 }
+
+/**
+  * Function to draw the plots.
+  *
+**/
+function drawPlots(){
+    if (plotsData["fGen"]){
+      document.querySelector("#reloadButton").style.visibility = "visible"; 
+      let TimePlotInput ={
+          divId : "timePlotIn", 
+          dataX : plotsData["fGen"]["t"],
+          dataY : plotsData["fGen"]["timeY"],
+          title : "Señal en el tiempo",
+          axisX : "t [s]",
+          axisY : "f(t) [V]", 
+      };
+      createPlot(TimePlotInput);
+      let FrecPlotInput ={
+        divId : "frecuencyPlotIn", 
+        dataX : plotsData["fGen"]["w"],
+        dataY : plotsData["fGen"]["frecY"],
+        title : "Señal en la frecuencia",
+        axisX : "w [Hz]",
+        axisY : "f(w) [V]",
+      };
+      createPlot(FrecPlotInput);
+    }
+
+    if (plotsData["fmso"]){
+      let TimePlotOut ={
+        divId : "timePlotOut", 
+        dataX : plotsData["fmso"]["t"],
+        dataY : plotsData["fmso"]["timeY"],
+        title : "Señal en el tiempo",
+        axisX : "t [s]",
+        axisY : "f(t) [V]", 
+      };
+      createPlot(TimePlotOut);
+      let FrecPlotOut ={
+        divId : "frecuencyPlotOut", 
+        dataX : plotsData["fmso"]["w"],
+        dataY : plotsData["fmso"]["frecY"],
+        title : "Señal en la frecuencia",
+        axisX : "w [Hz]",
+        axisY : "f(w) [V]",
+      };
+      createPlot(FrecPlotOut);
+    } 
+
+    if(plotsData["average"]){
+      let timePlotAverage ={
+        divId : "timePlotAverage", 
+        dataX : plotsData["average"]["t"],
+        dataY : plotsData["average"]["timeY"],
+        title : "Señal en el tiempo",
+        axisX : "t [s]",
+        axisY : "f(t) [V]", 
+      };
+      createPlot(timePlotAverage);
+    }
+};
 /**/////////////////////////////////////////////
 //////////                       ///////////////
 //////////  DOWNLOAD FUNCTIONS   ///////////////
 //////////                       ///////////////
 /////////////////////////////////////////////**/
 
-function saveFile(name, type){
-    const element = document.createElement('a');
+/**
+  * Function to download data send by backend.
+  *
+**/
+function saveFile(){
+    const name = document.querySelector("input#id_nameFile").value,
+          type = document.querySelector("select.input-style").value,
+          element = document.createElement("a");
     let file;
 
-    console.log(backData)
+    console.log(plotsData)
     switch(type){
-      case 'time':
-        file = new Blob(backData["fmso"] ? [backData["fmso"]["timeY"]]: ['No data'], {type: "text/plain"});
+      case "time":
+        file = new Blob(plotsData["fmso"] ? [JSON.stringify(plotsData)]: ["No data"], {type: "text/plain"});
         break;
-      case 'frec':
-        file = new Blob(backData["fmso"] ? [backData["fmso"]["frecY"]]: ['No data'], {type: "text/plain"});
+      case "frec":
+        file = new Blob(plotsData["fmso"] ? [mergeArrays(plotsData["fmso"]["frecY"], plotsData["fmso"]["frecY"]).join("\n")]: ["No data"], {type: "text/plain"});
         break;
-      case 'average':
-        file = new Blob(backData["average"] ? [backData["average"]["timeY"]]: ['No data'], {type: "text/plain"});
+      case "average":
+        file = new Blob(plotsData["average"] ? [mergeArrays(plotsData["fmso"]["frecY"], plotsData["fmso"]["frecY"]).join("\n")]: ["No data"], {type: "text/plain"});
         break;
     }
 
@@ -205,25 +244,43 @@ function saveFile(name, type){
     element.click();
 }
 
+/**
+  * Function to merge two arrays into one array (array[i] = "arrayX[i] arrayY[i]").
+  *
+  * @param {object} arrayX Array with the x values.
+  * @param {object} arraY Array with the y values.
+**/
+function mergeArrays(arrayX, arraY){
+  let data = [];
+  arrayX.map(function(element, position){ 
+    data.push(element + " " + arraY[position]);
+  });
+  return data;
+}
 /**/////////////////////////////////////////////
-//////////                       ///////////////
-//////////  COOKIES FUNCTIONS    ///////////////
-//////////                       ///////////////
+//////////                         ///////////////
+////////// LOCAL STORAGE FUNCTIONS ///////////////
+//////////                         ///////////////
 /////////////////////////////////////////////**/
 
-function setCokie(name, value) {
-    document.cookie = name + "=" + value + "; path=/; expires=" + expiry.toGMTString();
+/**
+  * Function to get information of the browser.
+  *
+  * @param {string} key data id.
+**/
+function getData(key){
+  let data = JSON.parse(localStorage.getItem(key))
+  return data ? data : {};
 };
 
-function getCookie(cname){
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-      var c = ca[i].trim();
-      if (c.indexOf(name)==0) {
-        return c.substring(name.length,c.length);
-      }
-    }
-    return "";
-}
+/**
+  * Function to set information in the browser.
+  *
+  * @param {string} key data id.
+  * @param {string} data data information.
+**/
+function setData(key, data){
+  localStorage.setItem(key, JSON.stringify(data));
+};
+
 
